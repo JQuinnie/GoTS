0e6a19ca2a214c32ca3705de889bbc6ac9c5f737
jquinnie <jenn8866@gmail.com>
2021-01-18 15:05:53 -0700

    Functions in interferences

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -4,10 +4,17 @@ interface Vehicle {
   isBroken: boolean;
 }
 
+interface Reportable {
+  summary(): string; // function that will return a string
+}
+
 const oldCivic = {
   name: 'civic',
   year: 2000,
   isBroken: true,
+  summary(): string {
+    return `This is ${this.name} and it is a ${this.year} ${this.isBroken ? 'broken' : 'working'} vehicle.`;
+  }
 };
 
 const printVehicle = (vehicle: { name: string; year: number; isBroken: boolean }): void => {
@@ -26,3 +33,20 @@ const anotherVehicle = (vehicle: Vehicle): void => {
 };
 
 anotherVehicle(oldCivic);
+
+const soda = {
+  brand: 'coke',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My ${this.brand} has ${this.sugar} grams of sugar.`
+  }
+}
+
+// encouraged to write somewhat generic functions
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+}
+
+printSummary(oldCivic);
+printSummary(soda);
